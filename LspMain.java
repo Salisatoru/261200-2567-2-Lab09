7701f675bdb1578e6745f441d361fbbe39f770ff
Salisatoru <95071118+Salisatoru@users.noreply.github.com>
2025-02-19 16:12:59 +0700

    Update LspMain.java

--- LspMain.java
+++ LspMain.java
@@ -1,12 +1,16 @@
-package lsp;
-
 public class LspMain {
     public static void main(String[] args) {
         Bird penguin = new Penguin();
         FlyingBird sparrow = new Sparrow();
+        Bird ostrich = new Ostrich(); // Add Ostrich
+        FlyingBird eagle = new Eagle(); // Add Eagle
 
         penguin.move();
         sparrow.move();
         sparrow.fly();
+        
+        ostrich.move();
+        eagle.move();
+        eagle.fly(); // Eagle can also fly
     }
 }
