178e038d5fd50559b365b7e0bf7acb86334a3f2d
Salisatoru <95071118+Salisatoru@users.noreply.github.com>
2025-02-19 16:11:19 +0700

    Update OcpMain.java

--- OcpMain.java
+++ OcpMain.java
@@ -4,9 +4,11 @@ public class OcpMain {
     public static void main(String[] args) {
         Shape rectangle = new Rectangle(5, 10);
         Shape circle = new Circle(7);
+        Shape ellipse = new Ellipse(5, 3); // Add Ellipse
 
         AreaCalculator calculator = new AreaCalculator();
         System.out.println("Rectangle Area: " + calculator.calculateArea(rectangle));
         System.out.println("Circle Area: " + calculator.calculateArea(circle));
+        System.out.println("Ellipse Area: " + calculator.calculateArea(ellipse)); // Print Ellipse Area
     }
 }
